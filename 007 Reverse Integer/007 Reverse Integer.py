86293c792f8da20f710dc3c1873b89b80fc78a8b
seanyuner <binzhu@hust.edu.cn>
2018-01-17 11:10:06 +0800

    Add files via upload

---
+++ 007 Reverse Integer/007 Reverse Integer.py
@@ -0,0 +1,25 @@
+# 45ms
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        n = cmp(x, 0) * int(str(abs(x))[::-1])
+        return n if n.bit_length() < 32 else 0
+
+
+# 53ms
+class Solution2(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        s = 0
+        while x:
+            s = s*10 + x%10
+            x = x/10
+        return sign*s if s < 2**31 else 0
